5bbe16b72de6e134b7ebbc85a1ac154aca728773
Job Vranish <job.vranish@atomicembedded.com>
2011-12-15 14:28:41 -0500

    initial commit. Adds initial build framework

---
+++ src/Language/TheExperiment.hs
@@ -0,0 +1,13 @@
+module Language.TheExperiment (
+    module Language.TheExperiment.AST,
+    module Language.TheExperiment.Lexer,
+    module Language.TheExperiment.Parser
+) where
+
+import Language.TheExperiment.AST
+import Language.TheExperiment.Lexer
+import Language.TheExperiment.Parser
+
+
+
+
---
+++ src/Language/TheExperiment/Parser.hs
@@ -0,0 +1,1 @@
+module Language.TheExperiment.Parser where
