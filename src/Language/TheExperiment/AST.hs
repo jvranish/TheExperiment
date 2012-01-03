3f3dfd83ed38bd92d5c4f978c7f92321481b5e18
John Van Enk <vanenkj@gmail.com>
2012-01-03 12:04:08 -0500

    Reflowed the spacing.

--- src/Language/TheExperiment/AST.hs
+++ src/Language/TheExperiment/AST.hs
@@ -21,79 +21,79 @@ data ParsedType = ParsedType { typePos    :: SourcePos
     deriving (Show, Eq, Ord)
 
 data NodeType a => Expr a
-            = Call              { exprPos      :: SourcePos
-                                , exprNodeData :: a
-                                , callFunc     :: Expr a
-                                , callParams   :: [Expr a]
-                                }
-            | Identifier        { exprPos      :: SourcePos
-                                , exprNodeData :: a
-                                , idName       :: String
-                                }
-            | Literal           { exprPos      :: SourcePos
-                                , exprNodeData :: a
-                                , literal      :: Literal
-                                }
-            | Member            { exprPos      :: SourcePos
-                                , exprNodeData :: a
-                                , memberExpr   :: Expr a
-                                , memberName   :: String
-                                }
+        = Call       { exprPos      :: SourcePos
+                     , exprNodeData :: a
+                     , callFunc     :: Expr a
+                     , callParams   :: [Expr a]
+                     }
+        | Identifier { exprPos      :: SourcePos
+                     , exprNodeData :: a
+                     , idName       :: String
+                     }
+        | Literal    { exprPos      :: SourcePos
+                     , exprNodeData :: a
+                     , literal      :: Literal
+                     }
+        | Member     { exprPos      :: SourcePos
+                     , exprNodeData :: a
+                     , memberExpr   :: Expr a
+                     , memberName   :: String
+                     }
     deriving (Show, Eq, Ord)
 
 data NodeType a => Statement a
-                 = Assign   { stmtPos      :: SourcePos
-                            , stmtNodeData :: a
-                            , assignName   :: String
-                            , assignExpr   :: Expr a
-                            }
-                 | If       { stmtPos      :: SourcePos
-                            , stmtNodeData :: a
-                            , ifCond       :: Expr a
-                            , ifThen       :: Statement a
-                            , ifElse       :: Maybe (Statement a)
-                            }
-                 | While    { stmtPos      :: SourcePos
-                            , stmtNodeData :: a
-                            , whileCond    :: Expr a
-                            , whileBody    :: Statement a
-                            }
-                 | ExprStmt { stmtPos      :: SourcePos
-                            , stmtNodeData :: a
-                            , stmtExpr     :: Expr a
-                            }
-                 | Return   { stmtPos      :: SourcePos
-                            , stmtNodeData :: a
-                            , returnExpr   :: Expr a
-                            }
-                 | Block    { stmtPos      :: SourcePos
-                            , stmtNodeData :: a
-                            , blockBody    :: ([TopLevelStmt a], [Statement a])
-                            }
+        = Assign   { stmtPos      :: SourcePos
+                   , stmtNodeData :: a
+                   , assignName   :: String
+                   , assignExpr   :: Expr a
+                   }
+        | If       { stmtPos      :: SourcePos
+                   , stmtNodeData :: a
+                   , ifCond       :: Expr a
+                   , ifThen       :: Statement a
+                   , ifElse       :: Maybe (Statement a)
+                   }
+        | While    { stmtPos      :: SourcePos
+                   , stmtNodeData :: a
+                   , whileCond    :: Expr a
+                   , whileBody    :: Statement a
+                   }
+        | ExprStmt { stmtPos      :: SourcePos
+                   , stmtNodeData :: a
+                   , stmtExpr     :: Expr a
+                   }
+        | Return   { stmtPos      :: SourcePos
+                   , stmtNodeData :: a
+                   , returnExpr   :: Expr a
+                   }
+        | Block    { stmtPos      :: SourcePos
+                   , stmtNodeData :: a
+                   , blockBody    :: ([TopLevelStmt a], [Statement a])
+                   }
     deriving (Show, Eq, Ord)
 
 data NodeType a => TopLevelStmt a
-                    = VarDef   { topStmtPos      :: SourcePos
-                               , topStmtNodeData :: a
-                               , varName         :: String
-                               , varInitExpr     :: Maybe (Expr a)
-                               }
-                    | ConstDef { topStmtPos      :: SourcePos
-                               , topStmtNodeData :: a
-                               , constName       :: String
-                               , constExpr       :: Expr a
-                               }
-                    | FuncDef  { topStmtPos      :: SourcePos
-                               , topStmtNodeData :: a
-                               , funcName        :: String
-                               , funcParams      :: [String]
-                               , funcStmt        :: Statement a
-                               }
-                    | TypeDef  { topStmtPos      :: SourcePos
-                               , topStmtNodeData :: a
-                               , typeDefName     :: String
-                               , typeDefType     :: ParsedType
-                               }
+        = VarDef   { topStmtPos      :: SourcePos
+                   , topStmtNodeData :: a
+                   , varName         :: String
+                   , varInitExpr     :: Maybe (Expr a)
+                   }
+        | ConstDef { topStmtPos      :: SourcePos
+                   , topStmtNodeData :: a
+                   , constName       :: String
+                   , constExpr       :: Expr a
+                   }
+        | FuncDef  { topStmtPos      :: SourcePos
+                   , topStmtNodeData :: a
+                   , funcName        :: String
+                   , funcParams      :: [String]
+                   , funcStmt        :: Statement a
+                   }
+        | TypeDef  { topStmtPos      :: SourcePos
+                   , topStmtNodeData :: a
+                   , typeDefName     :: String
+                   , typeDefType     :: ParsedType
+                   }
     deriving (Show, Eq, Ord)
                     
 
