fb46879e9923eef8f2f06afffa99a0c85910a823
John Van Enk <vanenkj@gmail.com>
2012-01-03 12:06:39 -0500

    Started adding a few planning docs to the parser.

--- src/Language/TheExperiment/Parser.hs
+++ src/Language/TheExperiment/Parser.hs
@@ -1,1 +1,17 @@
 module Language.TheExperiment.Parser where
+
+{- General structure of types:
+ - 
+ - Module
+ - \
+ -  TopLevelStmt
+ -   \
+ -    Statement
+ -    \
+ -     Expr
+ - 
+ - The type parameter to Module, TopLevelStmt, Statement, and Expr is () for
+ - the parser stage. This information is filled in later by the inference
+ - engine and the type checker.
+ -
+ -}
