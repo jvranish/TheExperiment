3ff7727b5ddcd92151032f0ceb44d83c44fd9bf5
John Van Enk <vanenkj@gmail.com>
2012-01-09 00:57:00 -0500

    Started expanding the parser. All literals added but float.

--- src/Language/TheExperiment/Parser.hs
+++ src/Language/TheExperiment/Parser.hs
@@ -15,3 +15,69 @@ module Language.TheExperiment.Parser where
  - engine and the type checker.
  -
  -}
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Data.Maybe
+import Numeric
+
+import Language.TheExperiment.AST
+
+readStdInt :: Num a => a -> String -> a
+readStdInt base str = val
+  where
+    [(val,_)] = readInt base validDigit digitValue str
+    digits = ['0'..'9'] ++ ['A'..'F']
+    values = [0..]
+    validDigit = (`elem` digits)
+    digitValue x = fromJust $ lookup x (zip digits values)
+
+aStringLiteral :: Parser Literal
+aStringLiteral = do
+  _ <- char '"'
+  s <- many $ noneOf "\""
+  _ <- char '"'
+
+  return $ StringLiteral s
+
+aCharLiteral :: Parser Literal
+aCharLiteral = do
+  _ <- char '\''
+  c <- noneOf "\'"
+  _ <- char '\''
+
+  return $ CharLiteral c
+
+aNumericLiteral :: Char -> [Char] -> (Integer -> Literal) -> Parser Literal
+aNumericLiteral prefix digits cons = do
+  _ <- char '0'
+  _ <- char prefix
+  lit <- many1 $ oneOf digits
+  return $ cons $ readStdInt (fromIntegral $ length digits) lit
+
+aBinLiteral :: Parser Literal
+aBinLiteral = aNumericLiteral 'b' ['0'..'1'] BinLiteral
+
+aHexLiteral :: Parser Literal
+aHexLiteral = aNumericLiteral 'x' (['0'..'9'] ++ ['A'..'F']) HexLiteral
+
+aOctalLiteral :: Parser Literal
+aOctalLiteral = aNumericLiteral 'o' ['0'..'7'] OctalLiteral
+
+aDecLiteral :: Parser Literal
+aDecLiteral = do
+  lit <- many1 $ oneOf ['0'..'9']
+  return $ IntegerLiteral $ readStdInt 10 lit
+
+aLiteral :: Parser Literal
+aLiteral = try aStringLiteral
+       <|> try aCharLiteral
+       <|> try aBinLiteral
+       <|> try aHexLiteral
+       <|> try aOctalLiteral
+       <|>     aDecLiteral
+
+parseExpr :: String -> String
+parseExpr input = case parse aLiteral "lisp" input of
+                    Left err -> "No match: " ++ show err
+                    Right val -> show val
+
