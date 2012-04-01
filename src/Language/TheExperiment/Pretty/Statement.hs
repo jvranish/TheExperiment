54ca832d2f62a928a992b4c23dadf9653d13a5a7
John Van Enk <vanenkj@gmail.com>
2012-04-01 18:22:43 -0400

    Stubbed Statement pretty printer.

---
+++ src/Language/TheExperiment/Pretty/Statement.hs
@@ -0,0 +1,10 @@
+module Language.TheExperiment.Pretty.Statement
+  ( prettyStatement
+  ) where
+
+
+import Text.PrettyPrint.HughesPJ
+import Language.TheExperiment.AST.Statement
+
+prettyStatement :: ParsedStatement -> Doc
+prettyStatement = undefined
